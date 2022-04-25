5f112a104883c36832a8d8d2efff7a14adb77a75
Erica Kido Shimomoto <ericaks@Ericas-MacBook-Pro.local>
2022-04-25 17:15:13 +0900

    Included download script to get data

--- download.sh
+++ download.sh
@@ -10,5 +10,5 @@ wget -P $DOWNLOAD_PATH/word_embeddings http://nlp.stanford.edu/data/glove.42B.30
 unzip $DOWNLOAD_PATH/word_embeddings/glove.42B.300d.zip
 
 # download dataset
-wget -P $DOWNLOAD_PATH/dataset https://www.dropbox.com/s/4au9bkv5xzi79ix/News2Meme.zip
-unzip $DOWNLOAD_PATH/dataset/News2Meme.zip
+wget -P $DOWNLOAD_PATH https://www.dropbox.com/s/4au9bkv5xzi79ix/News2Meme.zip
+unzip $DOWNLOAD_PATH/News2Meme.zip
